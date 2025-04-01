5c01abaed73e8829f89e33f7d01af63358e7529f
user <hikuj0212@gmail.com>
2025-04-01 10:36:51 +0900

    반복문과 파이구하기

--- p01pi.py
+++ p01pi.py
@@ -14,12 +14,9 @@ p = 1
 for n in range(1,100):
     p = p * ((2 * n + 1) ** 2 - 1) / (2 * n + 1) ** 2
 
-print(p*4)
+print(p * 4)
 
 import matplotlib.pyplot as plt
 plt.plot([1,3,4])
 plt.show()
 
-# import opencv
-#
-# opencv.
