2989dc122a3c52d0c1648d03bc187e86de660400
user <hikuj0212@gmail.com>
2025-04-30 16:16:46 +0900

    객체지향실습1

--- 06soccerclass.py
+++ 06soccerclass.py
@@ -1,4 +1,6 @@
 # 2025. 04. 29 python 수업 객체지향 프로그래밍 실습
+from PIL.ImImagePlugin import number
+
 
 class SoccerPlayer(object):
 
@@ -14,6 +16,9 @@ class SoccerPlayer(object):
         print(f'선수 번호 교체: {self.back_number} => {new_number}')
         self.back_number = new_number
 
+    def __str__(self):
+        return f"저의 이름은 {self.name}, 위치는 {self.position}, 번호는 {self.back_number} 입니다. 화이팅"
+
 # SoccerPlayer 클래스가 jh 오브젝트를 생성함
 # 클래스의 생성함수 constructor가 실행됨
 jh = SoccerPlayer("김종현", "MF", 10)
@@ -25,3 +30,20 @@ print(jh.name)
 jh.change_back_number(5)
 
 print(f'{jh.back_number}')
+
+names = ["Messi", "Ramos", "Ronaldo", "Park", "Son"]
+positions = ["MF", "DF", "CF", "WF", "GK"]
+numbers = [10, 9, 8, 7, 1]
+
+players = [[name, position, number] for name, position, number in zip(names, positions, numbers)]
+print(players)
+print(players[0])
+
+messi = SoccerPlayer(players[0][0], players[0][1], players[0][2])
+print(messi.name)
+
+player_oject = [SoccerPlayer(name, position, number) for name, position, number in zip(names, positions, numbers)]
+print(player_oject[0])
+
+print(player_oject[0].name)
+
